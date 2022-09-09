dc10b5c379e2dda1ec60085af4e70e0b6c782411
walmho <jones.ryan.t.01@gmail.com>
2022-09-08 17:16:51 -0700

    made cleaning functions for datasets. Most likely won't do anything because my practice one was from Kaggle so it probably doesn't have errors but still good practice

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import matplotlib as plt
+import scrubFunctions as scrub
+
+#https://www.w3schools.com/python/pandas/pandas_cleaning_empty_cells.asp
+#Pretty cool stuff
+
+raw = scrub.load("survey lung cancer.csv")
+df = scrub.clean(raw)
+
+
---
+++ scrubFunctions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+#This file will probably do nothing because this dataset came from kaggle but it's good to learn how to clean them anyways
+
+def load(file):
+    dataFile = pd.read_csv(file)
+    return dataFile
+
+def clean(file):
+    #Looks for bad pieces of data and removes them automagically
+    noNull = file.dropna()
+    #^make inplace=True if you want to edit the original dataset. This currently creates a new var with no null values
+    if file.duplicated():
+        cleaned = noNull.drop_duplicates()
+    return cleaned
+
+#can also later make functions to replace empty values with averages rather then just delete them, detect misentered data, etc.
