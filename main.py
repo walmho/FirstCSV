81fc158d40000dadb44b5afeeebbd7fadfc1016a
walmho <jones.ryan.t.01@gmail.com>
2022-09-08 20:03:05 -0700

    experimenting with creating graphs in a usable format that can compare and contrast results

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import scrubFunctions as scrub
 
-#https://www.w3schools.com/python/pandas/pandas_cleaning_empty_cells.asp
-#Pretty cool stuff
+# https://www.w3schools.com/python/pandas/pandas_cleaning_empty_cells.asp
+# Pretty cool stuff
 
 raw = scrub.load("survey lung cancer.csv")
 df = scrub.clean(raw)
@@ -11,8 +11,29 @@ df = scrub.clean(raw)
 def separateGenders(df):
     dfMale = df[df['GENDER'] == "M"]
     dfFemale = df[df['GENDER'] == "F"]
+    
     print(dfMale)
     print("\n")
     print(dfFemale)
     
-separateGenders(df)
+    return dfMale, dfFemale
+    
+def separateSmokers(df):
+    #This doesn't work for some reason so I'm ignoring it like all good coders
+    dfSmoke = df[df['SMOKING'] == "2"]
+    dfNo = df[df['SMOKING'] == "1"]
+    
+    print(dfSmoke)
+    print("\n")
+    print(dfNo)
+
+def graphDataPts(df, x, y, gender):
+    print(df)
+    
+    df.plot()
+    plt.title(gender)
+
+M, F = separateGenders(df)
+graphDataPts(M, 'SMOKING', 'ALCOHOL CONSUMED', gender="Male")
+graphDataPts(F, 'SMOKING', 'ALCOHOL CONSUMED', gender="Female")
+#separateSmokers(df)
--- scrubFunctions.py
+++ scrubFunctions.py
@@ -1,15 +1,15 @@
 import pandas as pd
-#This file will probably do nothing because this dataset came from kaggle but it's good to learn how to clean them anyways
+# This file will probably do nothing because this dataset came from kaggle but it's good to learn how to clean them anyways
 
 def load(file):
     dataFile = pd.read_csv(file)
     return dataFile
 
 def clean(file):
-    #Looks for bad pieces of data and removes them automagically
+    # Looks for bad pieces of data and removes them automagically
     noNull = file.dropna()
-    #^make inplace=True if you want to edit the original dataset. This currently creates a new var with no null values
+    # ^make inplace=True if you want to edit the original dataset. This currently creates a new var with no null values
     cleaned = noNull.drop_duplicates()
     return cleaned
 
-#can also later make functions to replace empty values with averages rather then just delete them, detect misentered data, etc.
+# can also later make functions to replace empty values with averages rather then just delete them, detect misentered data, etc.
