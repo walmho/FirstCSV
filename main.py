d5f8ea2a2784ad4da98d980929911e5423bb5bc1
walmho <jones.ryan.t.01@gmail.com>
2022-09-15 14:08:10 -0700

    being brutally honest I don't remember what I did I just need to push before I switch to a different project for a bit :)

--- main.py
+++ main.py
@@ -16,38 +16,28 @@ def separateGenders(df):
     dfFemale = df[df["GENDER"] == "F"]
 
     return dfMale, dfFemale
-    
-def separateSmokers(df):
-    #This doesn"t work for some reason so I"m ignoring it like all good coders
-    dfSmoke = df[df["SMOKING"] == "2"]
-    dfNo = df[df["SMOKING"] == "1"]
-    
-    print(dfSmoke)
-    print("\n")
-    print(dfNo)
 
-def graphDataPts(w):
-    #1) Isolate and create two new subsets within each gender: those who got lung cancer and those who didn"t
-    #2) Make a plot for each subset showing how many inLUNG_CANCER either subset had or didn"t have an attribute
-
+def plotOccurences():
+    #Graphs # of times patients of age X had lung cancer for both males and females
     occurenceCtM = (M["AGE"].value_counts()).sort_index(axis=0, ascending=True, inplace=False)
     plt.figure(0)
     occurenceCtM.plot(kind="bar", color="blue")
     plt.title("Lung Cancer in Males")
     plt.xlabel("Age")
-    plt.ylabel("Logged lung cancer")
+    plt.ylabel("# of Cases")
     
     occurenceCtF = (F["AGE"].value_counts()).sort_index(axis=0, ascending=True, inplace=False)
     plt.figure(1)
     occurenceCtF.plot(kind="bar", color="pink")
     plt.title("Lung Cancer in Females")
     plt.xlabel("Age")
-    plt.ylabel("Logged lung cancer")
+    plt.ylabel("# of Cases")
 
+#converts yes/no values to 1 or 2 for graphability
 df["LUNG_CANCER"] = df["LUNG_CANCER"].replace({"YES": "2"})
 df["LUNG_CANCER"] = df["LUNG_CANCER"].replace({"NO": "1"})
 
 M, F = separateGenders(df)
 
-graphDataPts(0.3)
+plotOccurences()
 plt.show()
